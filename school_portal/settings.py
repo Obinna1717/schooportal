1657fb0573abeea06876004318d74e753ef842a7
Obinna <ugwokeobinna52@gmail.com>
2026-07-06 14:09:49 +0100

    Configure project for Render deployment

--- school_portal/settings.py
+++ school_portal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)2z*e$hjm(2fjmughdld-22y9&23yg$b1fl(nk=q%(i@_ja+-#'
+# SECRET_KEY = 'django-insecure-)2z*e$hjm(2fjmughdld-22y9&23yg$b1fl(nk=q%(i@_ja+-#'
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-)2z*e$hjm(2fjmughdld-22y9&23yg$b1fl(nk=q%(i@_ja+-#",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
@@ -82,10 +92,10 @@ WSGI_APPLICATION = 'school_portal.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
