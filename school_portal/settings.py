8ff9f7f414c5d86766bd530a7a6a027de2d1df1b
Obinna1717 <ugwokeobinna52@gmail.com>
2026-07-15 02:52:46 +0100

    Update settings.py

--- school_portal/settings.py
+++ school_portal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
