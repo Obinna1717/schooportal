68c4c4f17fb832376abf51052b95911399594ee5
Obinna <ugwokeobinna52@gmail.com>
2026-07-17 11:46:20 +0100

    Fix accounts/models

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     ROLE_CHOICES = (
         ("student", "Student"),
-        ("lecturer", "Lecturer"),
+        # ("lecturer", "Lecturer"),
     )
 
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="student")
